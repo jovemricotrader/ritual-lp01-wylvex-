879f3e21ad4c42fc4dc267237d98160ca12ea882
jovemricotrader <canalsemcueca@gmail.com>
2026-04-05 02:03:31 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import{defineConfig}from'vite'
 import react from'@vitejs/plugin-react'
-export default defineConfig({plugins:[react()],base:'/',build:{outDir:'dist',minify:false}})
+export default defineConfig({plugins:[react()],base:'/',build:{outDir:'dist',minify:true}})
